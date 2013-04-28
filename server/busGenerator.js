c982058c3fe92731b037efa3eb9f0c848a66cf89
Sidney <sidney@sidney15r.(none)>
2013-04-28 11:27:49 -0400

    Removed a emitter constraint.

--- server/busGenerator.js
+++ server/busGenerator.js
@@ -6,7 +6,6 @@ var ticksInASecond = 1000; // A second in milliseconds.
 var updateInterval = 1 * ticksInASecond;
 var createTime = Date.now().toString();
 
-client.setMaxListeners(50);
 function updateBusData() {
     restRequest.get(martaDataURI).on('complete', function(data) {
         data.forEach(function(busInfo) {
