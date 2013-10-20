acd720cfe330d9bf36a0a98af6f5ae00b045c0e7
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2013-10-20 14:04:40 -0400

    Formated info-bar into two sections.[1;5D

--- load.js
+++ load.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
     //$('#info-bar').hide();
+    //$('#bus-search').hide();
+    //$('#bus-info-border').hide();
 
     $('#click-bar').click(function () {
         $('#info-bar').slideToggle("slow");
+        $('#bus-search').slideToggle("slow");
+        $('#bus-info-border').slideToggle("slow");
     });
 });
