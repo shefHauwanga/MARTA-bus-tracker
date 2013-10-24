6a3a55a6b9f6da79dfdd08f7c67d9f39b252808c
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2013-10-23 23:28:36 -0400

    Added functionality to select individual buses.

--- server/busWebServer.js
+++ server/busWebServer.js
@@ -1,11 +1,21 @@
 var webServer = require("http");
 var redis = require("redis");
+var url = require('url');
 var client = redis.createClient();
 var listenPort = 8888;
 var buses = [];
 
 webServer.createServer(function(request, response) {
     var busObj = [];
+    var busNum;
+    var queryData = url.parse(request.url, true).query;    
+
+    if (queryData.bus) {
+        busNum = queryData.bus;
+    } else {
+        busNum = "*";
+    }
+
     client.keys("*", function(error, replies) {
         replies.forEach(function(keyVal) {
             client.get(keyVal.toString(), function(err, reply) {
