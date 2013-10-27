8929f73cdbf8bd2547961c8534919bf5c2b20791
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2013-10-27 12:55:29 -0400

    Added async.js to busGenerator.js

--- server/busGenerator.js
+++ server/busGenerator.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var restRequest = require('restler');
 var redis = require("redis");
 var client = redis.createClient();
@@ -7,52 +8,64 @@ var updateInterval = 10 * 1000;
 function updateBusData() {
     restRequest.get(martaDataURI).on('success', function(data) {
         var createTime = Date.now().toString();
-        data.forEach(function(busInfo) {
-            client.get(busInfo.VEHICLE, function(err, reply) {
-                if(reply === null) {
-                    var busData = {
-                        id: busInfo.VEHICLE,
-                        route: busInfo.ROUTE,
-                        latitude: busInfo.LATITUDE,
-                        longitude: busInfo.LONGITUDE,
-                        direction: busInfo.DIRECTION,
-                        nextStop: busInfo.TIMEPOINT,
-                        adherence: busInfo.ADHERENCE,
-                        trip: busInfo.TRIPID,
-                        creationTime: createTime
-                    };
-                } else {
-                    reply = JSON.parse(reply);
-                    if(busInfo.LATITUDE !== reply.latitude ||
-                       busInfo.LONGITUDE !== reply.longitude) {
-                        var busData = reply;
-                        busData.latitude = busInfo.LATITUDE;
-                        busData.longitude = busInfo.LONGITUDE;
-                        busData.creationTime = createTime;
+        async.forEach(data, function(busInfo, callback) {
+            var bus_data = null;
+            async.series([
+                function(inner_callback){
+                    client.get(busInfo.VEHICLE, function(err, reply) {
+                        if(reply === null) {
+                            bus_data = {
+                                id: busInfo.VEHICLE,
+                                route: busInfo.ROUTE,
+                                latitude: busInfo.LATITUDE,
+                                longitude: busInfo.LONGITUDE,
+                                direction: busInfo.DIRECTION,
+                                nextStop: busInfo.TIMEPOINT,
+                                adherence: busInfo.ADHERENCE,
+                                trip: busInfo.TRIPID,
+                                creationTime: createTime
+                            };
+                        } else {
+                            reply = JSON.parse(reply);
+                            if(busInfo.LATITUDE !== reply.latitude ||
+                              busInfo.LONGITUDE !== reply.longitude) {
+                                bus_data = reply;
+                                bus_data.latitude = busInfo.LATITUDE;
+                                bus_data.longitude = busInfo.LONGITUDE;
+                                bus_data.creationTime = createTime;
 
-                        if(busInfo.DIRECTION !== busData.direction)
-                            busData.direction = busInfo.DIRECTION;
+                                if(busInfo.DIRECTION !== bus_data.direction)
+                                    bus_data.direction = busInfo.DIRECTION;
 
-                        if(busInfo.ROUTE !== busData.route)
-                            busData.route = busInfo.ROUTE;
+                                if(busInfo.ROUTE !== bus_data.route)
+                                    bus_data.route = busInfo.ROUTE;
 
-                        if(busInfo.ADHERENCE !== busData.adherence)
-                            busData.adherence = busInfo.ADHERENCE;
+                                if(busInfo.ADHERENCE !== bus_data.adherence)
+                                    bus_data.adherence = busInfo.ADHERENCE;
 
-                        if(busInfo.TIMEPOINT !== busData.nextStop)
-                            busData.nextStop = busInfo.TIMEPOINT;
+                                if(busInfo.TIMEPOINT !== bus_data.nextStop)
+                                    bus_data.nextStop = busInfo.TIMEPOINT;
 
-                        if(busInfo.TRIPID !== busData.trip)
-                            busData.trip = busInfo.TRIPID;
-                    }
+                                if(busInfo.TRIPID !== bus_data.trip)
+                                    bus_data.trip = busInfo.TRIPID;
+                            }
+                        }
+
+                        inner_callback();
+                    });
+                }, function(inner_callback){
+                    if(bus_data !== null)
+                        client.set(bus_data.id, JSON.stringify(bus_data));
+
+                    inner_callback();
                 }
-                if(busData !== undefined)
-                    client.set(busData.id, JSON.stringify(busData));
+            ], function(err){
+                callback();
             });
+            setTimeout(function() {
+                updateBusData();
+            }, updateInterval);
         });
-        setTimeout(function() {
-            updateBusData();
-        }, updateInterval);
     });
 }
 updateBusData();
