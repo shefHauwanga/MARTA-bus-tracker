1b4cbd1dd6200f737890864a3b47d104d20737ca
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2013-10-27 11:51:11 -0400

    Added async.js to busWebServer.js

--- server/busWebServer.js
+++ server/busWebServer.js
@@ -1,35 +1,38 @@
+var async = require("async");
 var webServer = require("http");
 var redis = require("redis");
 var url = require('url');
 var client = redis.createClient();
 var listenPort = 8888;
-var buses = [];
 
 webServer.createServer(function(request, response) {
-    var busObj = [];
-    var busNum;
-    var queryData = url.parse(request.url, true).query;    
+    if(request.url !== "/favicon.ico"){
+        var buses = [];
+        var queryData = url.parse(request.url, true).query;    
 
-    if (queryData.bus) {
-        busNum = queryData.bus;
-    } else {
-        busNum = "*";
-    }
+        if (queryData.bus) {
+            busNum = queryData.bus;
+        } else {
+            busNum = "*";
+        }
 
-    client.keys("*", function(error, replies) {
-        replies.forEach(function(keyVal) {
-            client.get(keyVal.toString(), function(err, reply) {
-                buses.push(JSON.parse(reply));
-            });
+        client.keys(busNum, function(error, replies) {
+            async.forEach(replies, function(keyVal, callback) {
+                client.get(keyVal.toString(), function(err, reply) {
+                    buses.push(JSON.parse(reply));
+
+                    callback();
+                });
+            }, function(err){
+                var headers = {
+                    'Content-Type': 'text/plain',
+                    'Content-Length': JSON.stringify(buses).length
+                };
+
+                response.writeHead(200, headers);
+                response.write(JSON.stringify(buses));
+                response.end();
+           });
         });
-    });
-    busObj = buses;
-    buses = [];
-    var headers = {
-        'Content-Type': 'text/plain',
-        'Content-Length': JSON.stringify(busObj).length
-    };
-    response.writeHead(200, headers);
-    response.write(JSON.stringify(busObj));
-    response.end();
+    }
 }).listen(listenPort);
