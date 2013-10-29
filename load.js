4274de060962273ef1c2ba8739aa6de9b4b9f394
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2013-10-28 22:18:08 -0400

    Fixed animation woes. Fix positioning for search bar.

--- load.js
+++ load.js
@@ -1,16 +1,7 @@
 $(document).ready(function(){
     $('#info-bar').hide();
-    $('#bus-search-form').hide();
 
     $('#click-bar').click(function () {
-        if($('#info-bar').css('display') === 'none') {
-            $('#info-bar').slideToggle("slow", function(){
-                $('#bus-search-form').slideToggle("slow");
-            });
-        } else {
-            $('#bus-search-form').slideToggle("slow", function(){
-                $('#info-bar').slideToggle("slow");
-            });
-        }
+        $('#info-bar').slideToggle("slow");
     });
 });
