df7d517781693df6d738d96f91c2b5e317235e09
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2013-11-11 17:49:35 -0500

    Made modifications to helper file for individual route data.

--- helper.php
+++ helper.php
@@ -1,5 +1,11 @@
 <?php
     header("Cache-Control: no-cache, must-revalidate");
-    $json = file_get_contents("http://localhost:8888");
+    if(isset($_GET['bus'])){
+        $json = file_get_contents("http://localhost:8888?bus=".$_GET['bus']);
+    } elseif(isset($_GET['trip_id'])){
+        $json = file_get_contents("http://localhost:8889?trip_id=".$_GET['trip_id']);
+    } else {
+        $json = file_get_contents("http://localhost:8888");
+    }
     echo $json;
 ?>
