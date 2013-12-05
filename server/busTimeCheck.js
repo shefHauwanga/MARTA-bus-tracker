4da20480c4368840cb55750c8872837dd9242ae9
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2013-12-05 12:13:01 -0500

    Changed response from stop_id to stop_name.

--- server/busTimeCheck.js
+++ server/busTimeCheck.js
@@ -20,32 +20,43 @@ webServer.createServer(function(request, response) {
         var time_re = new RegExp("(\d{2}):(\d{2}):(\d{2})\s(AM|PM)/");
         var current_stop = "0";
 
-        db.stop_times.find({trip_id: query_data.time_id}).sort({stop_sequence:1}, function(err, stops_with_time){
-            async.forEach(stops_with_time, function(stop_with_time, callback) {
-                time_captures = stop_with_time.match(re);
+        async.series([
+            function(callBack){
+                db.stop_times.find({trip_id: query_data.time_id}).sort({stop_sequence:1}, function(err, stops_with_time){
+                    async.forEach(stops_with_time, function(stop_with_time, innerCallback) {
+                        time_captures = stop_with_time.match(re);
 
-                if(time_captures[4] !== a_o_p) {
-                    if(a_o_p === "PM")
-                        current_stop = stop_with_time.stop_id;
-                } else if(parseInt(time_captures[1]) > current_hour) {
-                        current_stop = stop_with_time.stop_id;
-                } else if(parseInt(time_captures[2]) > current_minutes) {
-                        current_stop = stop_with_time.stop_id;
-                } else if(parseInt(time_captures[3]) > current_second) {
-                        current_stop = stop_with_time.stop_id;
-                }
+                        if(time_captures[4] !== a_o_p) {
+                            if(a_o_p === "PM")
+                                current_stop = stop_with_time.stop_id;
+                        } else if(parseInt(time_captures[1]) > current_hour) {
+                            current_stop = stop_with_time.stop_id;
+                        } else if(parseInt(time_captures[2]) > current_minutes) {
+                            current_stop = stop_with_time.stop_id;
+                        } else if(parseInt(time_captures[3]) > current_second) {
+                            current_stop = stop_with_time.stop_id;
+                        }
 
+                        innerCallback();
+                    }, function(err){
+                        callback();
+                    });
+                });
+            }, 
+            function(callBack){
+                db.stops.find({stop_id: current_stop}, function(err, stop){
+                    current_stop_name = stop[0].stop_name;
+                });
                 callback();
-            }, function(err){
-                   var headers = {
-                       'Content-Type': 'text/plain',
-                       'Content-Length': JSON.stringify({"next_stop_id": current_stop}).length
-                   };
+            }, 
+        ], function(err){
+            var headers = {
+                'Content-Type': 'text/plain',
+                'Content-Length': JSON.stringify({"next_stop": current_stop_name}).length
+            };
  
-                   response.writeHead(200, headers);
-                   response.write({"next_stop_id": current_stop});
-                   response.end();
-            });
+            response.writeHead(200, headers);
+            response.write({"next_stop": current_stop_name});
+            response.end();*/
         });
-    }
 }).listen(listenPort);
