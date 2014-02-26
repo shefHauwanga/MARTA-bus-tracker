41ae7b81e8fd4d5ebe5a640a26584c9248f9e4b7
Sheefeni Hauwanga <shef.hauwanga@gmail.com>
2014-02-26 13:15:25 -0500

    Fixed routes and moved buses templates out of main template.

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -1,18 +1,27 @@
 Buses.Router.map(function () {
-  this.resource('buses', { path: '/' }, function() {
+  this.resource('list', function() {
     // additional child routes    
+    this.route('buses');
+    this.resource('trip', {path: '/trip/:trip_id'}, function() {
+      this.route('bus', {path: '/bus/:bus_id'});
+    });
   });
-  //this.route('buses');
 });
 
-/*Buses.IndexRoute = Ember.Route.extend({
+Buses.IndexRoute = Ember.Route.extend({
   redirect: function() {
-    this.transitionTo('buses');
+    this.transitionTo('list.buses');
   }
-});*/
+});
 
-Buses.BusesRoute = Ember.Route.extend({
+Buses.ListBusesRoute = Ember.Route.extend({
   model: function () {
     return this.store.find('bus');
+  }
+});
+
+Buses.ListTripRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('trip', {trip: 'trip_id', bus: 'bus_id'});
   }
 });
